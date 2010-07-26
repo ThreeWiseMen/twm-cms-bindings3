7c5fe99479630930c5f9894bf8fd8aa612904487
Steven Vetzal <steve@vetzal.com>
2010-07-26 14:08:09 -0400

    a bit of refactoring to make rails happier

--- Rakefile.rb
+++ Rakefile.rb
@@ -12,13 +12,13 @@ require 'rake/gempackagetask'
 spec = Gem::Specification.new do |s|
 	s.platform          = Gem::Platform::RUBY
         s.name              = "twm-cms-bindings"
-        s.version           = "3.0.0"
+        s.version           = "3.0.1"
         s.author            = "Three Wise Men"
         s.email             = "info @nospam@ threewisemen.ca"
         s.summary           = "Binding library for TWM CMS v3 platform"
+        s.description       = "Use this library in a Ruby application to draw content from the TWM CMS v3 platform."
         s.files             = FileList[ 'lib/*.rb', 'test/*'].to_a
         s.require_path      = "lib"
-        s.autorequire       = "memcache-client"
         s.test_files        = Dir.glob('test/*.rb')
         s.has_rdoc          = true
         s.extra_rdoc_files  = ["README"]
